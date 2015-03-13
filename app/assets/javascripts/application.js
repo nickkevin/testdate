904bd2ce1cdcbbec997208d99c5d456dd5a644c6
nickkevin <nickkevin35@gmail.com>
2015-03-13 07:25:05 +0000

    fixed the datepicker issue

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,9 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap-datepicker
+
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-datepicker
 //= require turbolinks
 //= require_tree .
